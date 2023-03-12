242bcb081103d00eaf454d23f9dff1b3a0c04514
Orlando Martinez <orjamar@gmail.com>
2023-03-11 18:03:37 -0600

    Creates function with loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const arr = [];
+    for (let y=0; y<names.length; y++) {
+        arr.push(`Thank you, ${names[y]}, for the wonderful surprise gift!`);
+    }
+    return arr;
+}
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+countDown();
